426b9c9ff34c519123af628daac47ef64ef05e85
harborgrid-justin <justin.saadein@harborgrid.com>
2023-08-22 19:23:19 -0400

    c

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const config = require('./config/config');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const _ = require('lodash');
 
 const app = express();
 
@@ -9,4 +13,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
-

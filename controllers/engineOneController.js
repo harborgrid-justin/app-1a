0696ed9d4620052fb867f010504d7980fa585277
harborgrid-justin <justin.saadein@harborgrid.com>
2023-08-27 14:05:00 -0400

    g

--- controllers/engineOneController.js
+++ controllers/engineOneController.js
@@ -2,60 +2,34 @@ const EngineOneService = require('../services/engineOneService');
 const logger = require('../utilities/logger');
 
 class EngineOneController {
-    static async getAll(req, res) {
+    static async handleResponse(promise, res) {
         try {
-            const records = await EngineOneService.fetchEngineOneData();
-            res.json(records);
+            const data = await promise;
+            res.json(data);
         } catch (err) {
-            logger.error(`Error fetching all records: ${err.message}`);
-            res.status(500).send(err.message);
+            logger.error(err.message);
+            res.status(err.statusCode || 500).send(err.message);
         }
     }
 
-    static async getById(req, res) {
-        try {
-            const record = await EngineOneService.fetchEngineOneData({ id: req.params.id });
-            if (!record) {
-                return res.status(404).send('Record not found');
-            }
-            res.json(record);
-        } catch (err) {
-            logger.error(`Error fetching record by ID: ${err.message}`);
-            res.status(500).send(err.message);
-        }
+    static getAll(req, res) {
+        this.handleResponse(EngineOneService.fetchEngineOneData(), res);
     }
 
-    static async create(req, res) {
-        try {
-            const newRecord = await EngineOneService.createEngineOneData(req.body);
-            res.status(201).json(newRecord);
-        } catch (err) {
-            logger.error(`Error creating new record: ${err.message}`);
-            res.status(400).send(err.message);
-        }
+    static getById(req, res) {
+        this.handleResponse(EngineOneService.fetchEngineOneData({ id: req.params.id }), res);
     }
 
-    static async update(req, res) {
-        try {
-            const updatedRecord = await EngineOneService.updateEngineOneData(req.params.id, req.body);
-            if (!updatedRecord) {
-                return res.status(404).send('Record not found');
-            }
-            res.json(updatedRecord);
-        } catch (err) {
-            logger.error(`Error updating record: ${err.message}`);
-            res.status(400).send(err.message);
-        }
+    static create(req, res) {
+        this.handleResponse(EngineOneService.createEngineOneData(req.body), res);
     }
 
-    static async delete(req, res) {
-        try {
-            await EngineOneService.deleteEngineOneData(req.params.id);
-            res.json({ message: 'Record deleted successfully' });
-        } catch (err) {
-            logger.error(`Error deleting record: ${err.message}`);
-            res.status(500).send(err.message);
-        }
+    static update(req, res) {
+        this.handleResponse(EngineOneService.updateEngineOneData(req.params.id, req.body), res);
+    }
+
+    static delete(req, res) {
+        this.handleResponse(EngineOneService.deleteEngineOneData(req.params.id), res);
     }
 }
 

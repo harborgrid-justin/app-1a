56511c11f2148ec318bba58171c959546267d5e3
harborgrid-justin <justin.saadein@harborgrid.com>
2023-08-27 14:03:11 -0400

    f

--- controllers/engineOneController.js
+++ controllers/engineOneController.js
@@ -1,62 +1,59 @@
-const EngineOneModel = require('./engineOneModel');
+const EngineOneService = require('../services/engineOneService');
+const logger = require('../utilities/logger');
 
 class EngineOneController {
-    // Fetch all records
     static async getAll(req, res) {
         try {
-            const records = await EngineOneModel.findAll();
+            const records = await EngineOneService.fetchEngineOneData();
             res.json(records);
         } catch (err) {
+            logger.error(`Error fetching all records: ${err.message}`);
             res.status(500).send(err.message);
         }
     }
 
-    // Fetch a single record by ID
     static async getById(req, res) {
         try {
-            const record = await EngineOneModel.findById(req.params.id);
+            const record = await EngineOneService.fetchEngineOneData({ id: req.params.id });
             if (!record) {
                 return res.status(404).send('Record not found');
             }
             res.json(record);
         } catch (err) {
+            logger.error(`Error fetching record by ID: ${err.message}`);
             res.status(500).send(err.message);
         }
     }
 
-    // Create a new record
     static async create(req, res) {
         try {
-            const newRecord = new EngineOneModel(req.body);
-            await newRecord.save();
+            const newRecord = await EngineOneService.createEngineOneData(req.body);
             res.status(201).json(newRecord);
         } catch (err) {
+            logger.error(`Error creating new record: ${err.message}`);
             res.status(400).send(err.message);
         }
     }
 
-    // Update a record by ID
     static async update(req, res) {
         try {
-            const updatedRecord = await EngineOneModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            const updatedRecord = await EngineOneService.updateEngineOneData(req.params.id, req.body);
             if (!updatedRecord) {
                 return res.status(404).send('Record not found');
             }
             res.json(updatedRecord);
         } catch (err) {
+            logger.error(`Error updating record: ${err.message}`);
             res.status(400).send(err.message);
         }
     }
 
-    // Delete a record by ID
     static async delete(req, res) {
         try {
-            const deletedRecord = await EngineOneModel.findByIdAndDelete(req.params.id);
-            if (!deletedRecord) {
-                return res.status(404).send('Record not found');
-            }
+            await EngineOneService.deleteEngineOneData(req.params.id);
             res.json({ message: 'Record deleted successfully' });
         } catch (err) {
+            logger.error(`Error deleting record: ${err.message}`);
             res.status(500).send(err.message);
         }
     }
